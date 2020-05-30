2b3479014abf4d91e093005a111df856a7eb04ac
tobiu <tobiasuhlig78@gmail.com>
2020-05-30 14:58:32 +0200

    MainContainerController.mjs => onSwitchThemeButtonClick()

--- apps/covid/view/MainContainerController.mjs
+++ apps/covid/view/MainContainerController.mjs
@@ -1,4 +1,5 @@
 import {default as ComponentController} from '../../../node_modules/neo.mjs/src/controller/Component.mjs';
+import NeoArray                         from '../../../node_modules/neo.mjs/src/util/Array.mjs';
 
 /**
  * @class Covid.view.MainContainerController
@@ -13,9 +14,58 @@ class MainContainerController extends ComponentController {
         className: 'Covid.view.MainContainerController',
     }}
 
-    constructor(config) {
-        super(config);
-        console.log('MainContainerController ctor');
+    /**
+     * @param {Object} data
+     */
+    onReloadDataButtonClick(data) {
+        console.log('onReloadDataButtonClick');
+    }
+
+    /**
+     * @param {Object} data
+     */
+    onSwitchThemeButtonClick(data) {
+        let me       = this,
+            button   = data.component,
+            logo     = me.getReference('logo'),
+            logoPath = 'https://raw.githubusercontent.com/neomjs/pages/master/resources/images/apps/covid/',
+            vdom     = logo.vdom,
+            view     = me.view,
+            buttonText, cls, iconCls, theme;
+
+        if (button.text === 'Theme Light') {
+            buttonText = 'Theme Dark';
+            iconCls    = 'fa fa-moon';
+            theme      = 'neo-theme-light';
+        } else {
+            buttonText = 'Theme Light';
+            iconCls    = 'fa fa-sun';
+            theme      = 'neo-theme-dark';
+        }
+
+        vdom.src = logoPath + (theme === 'neo-theme-dark' ? 'covid_logo_dark.jpg' : 'covid_logo_light.jpg');
+        logo.vdom = vdom;
+
+        button.set({
+            iconCls: iconCls,
+            text   : buttonText
+        });
+
+        cls = [...view.cls];
+
+        view.cls.forEach(item => {
+            if (item.includes('neo-theme')) {
+                NeoArray.remove(cls, item);
+            }
+        });
+
+        NeoArray.add(cls, theme);
+        view.cls = cls;
+
+        button.set({
+            iconCls: iconCls,
+            text   : buttonText
+        });
     }
 }
 
